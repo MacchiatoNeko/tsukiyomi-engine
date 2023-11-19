d4dc8845d58c7683e6740ce72c992c01e099b88d
AtomSnow <51972322+AtomSnow@users.noreply.github.com>
2023-11-19 23:28:55 +0100

    wpf

--- TsukiyomiEngine/TsukiyomiEditor/MainWindow.xaml.cs
+++ TsukiyomiEngine/TsukiyomiEditor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TsukiyomiEditor.ProjectBrowser;
 
 namespace TsukiyomiEditor
 {
@@ -23,6 +24,25 @@ namespace TsukiyomiEditor
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenWindow();
+        }
+
+        private void OpenWindow()
+        {
+            var window = new ProjectBrowserWindow();
+            if (window.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                // Fill later
+            }
         }
     }
 }
